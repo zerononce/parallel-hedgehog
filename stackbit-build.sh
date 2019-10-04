0b507e2c718104e1137397ad75ec52f3e611bcad
Stackbit <projects@stackbit.com>
2019-10-04 16:19:38 +0000

    initial commit by Stackbit

---
+++ stackbit-build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -v
+
+npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5d9770f1f6fc42001c491a41
+npm install && npm run build
